5864ba3b3443f93a31e5e2abf213a2cd74586751
iamheetp <167682034+iamheetp@users.noreply.github.com>
2024-10-17 23:26:10 +0000

    plaid connected
    still need to store public token to state variable

--- client/learning_plaid/src/App.jsx
+++ client/learning_plaid/src/App.jsx
@@ -1,36 +1,32 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import { usePlaidLink } from "react-plaid-link";
+
+axios.defaults.baseURL = "http://localhost:8000";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
+  const [linkToken, setLinkToken] = useState();
   useEffect(() => {
-    fetch("http://localhost:5000/api")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setBackendData(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setBackendData({ error: "Failed to fetch data" });
-      });
+    async function fetch() {
+      const response = await axios.post("/create_link_token");
+      setLinkToken(response.data.link_token);
+    }
+    fetch();
   }, []);
 
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+      console.log("success", public_token, metadata);
+      // send public_token to server
+    },
+  });
+
   return (
-    <>
-      <div>
-        {typeof backendData.users === "undefined" ? (
-          <p>Loading</p>
-        ) : (
-          backendData.users.map((user, i) => <p key={i}>{user}</p>)
-        )}
-      </div>
-    </>
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,47 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
 
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": "67117039751213001a42abc6",
+      "PLAID-SECRET": "b82711b5ead9c75d9acd0675f3f1c3",
+    },
+  },
+});
+const plaidClient = new PlaidApi(configuration);
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/api", (req, res) => {
-  console.log("ko");
-  res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
+app.post("/hello", (request, response) => {
+  response.json({ message: "hello " + request.body.name });
 });
 
-app.listen(5000, () => {
+app.post("/create_link_token", async function (request, response) {
+  const plaidRequest = {
+    user: {
+      // This should correspond to a unique id for the current user.
+      client_user_id: "user",
+    },
+    client_name: "Plaid Test App",
+    products: ["auth"],
+    language: "en",
+    redirect_uri: "http://localhost:5173/",
+    country_codes: ["US"],
+  };
+  try {
+    const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+    response.json(createTokenResponse.data);
+  } catch (error) {
+    // handle error
+    response.status(500).send("failure");
+  }
+});
+
+app.listen(8000, () => {
   console.log("server started on port 5000");
 });
