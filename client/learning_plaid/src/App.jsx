939332cffdaf6008bb230e22152a8088f82d3a5a
iamheetp <167682034+iamheetp@users.noreply.github.com>
2024-10-18 14:38:09 +0000

    finished

--- client/learning_plaid/src/App.jsx
+++ client/learning_plaid/src/App.jsx
@@ -5,8 +5,35 @@ import { usePlaidLink } from "react-plaid-link";
 
 axios.defaults.baseURL = "http://localhost:8000";
 
+function PlaidAuth({ publicToken }) {
+  const [account, setAccount] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      let accessToken = await axios.post("/exchange_public_token", {
+        public_token: publicToken,
+      });
+      console.log("accessToken", accessToken.data);
+      const auth = await axios.post("/auth", {
+        access_token: accessToken.data.accessToken,
+      });
+      console.log("auth data", auth.data);
+      setAccount(auth.data.numbers.ach[0]);
+    }
+    fetchData();
+  }, []);
+  return (
+    account && (
+      <>
+        <p>Account number:{account.account}</p>
+        <p>Routing number:{account.routing}</p>
+      </>
+    )
+  );
+}
+
 function App() {
   const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
   useEffect(() => {
     async function fetch() {
       const response = await axios.post("/create_link_token");
@@ -18,12 +45,15 @@ function App() {
   const { open, ready } = usePlaidLink({
     token: linkToken,
     onSuccess: (public_token, metadata) => {
+      setPublicToken(public_token);
       console.log("success", public_token, metadata);
       // send public_token to server
     },
   });
 
-  return (
+  return publicToken ? (
+    <PlaidAuth publicToken={publicToken} />
+  ) : (
     <button onClick={() => open()} disabled={!ready}>
       Connect a bank account
     </button>
