3e16f8cf542c5cc76686a3a10127a413c61cc69c
iamheetp <167682034+iamheetp@users.noreply.github.com>
2025-01-16 16:20:18 +0000

    changes in app.jsx

--- client/learning_plaid/src/App.jsx
+++ client/learning_plaid/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css";
+
 import { usePlaidLink } from "react-plaid-link";
 
 axios.defaults.baseURL = "http://localhost:8000";
